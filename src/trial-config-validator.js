902d18d6abef46c2562084a780a6bfd1f1d78188
Cherrie Chang <cchang@vassar.edu>
2025-05-14 22:37:48 -0400

    add predicateArray to schema, export validator as function

--- src/trial-config-validator.js
+++ src/trial-config-validator.js
@@ -2,21 +2,21 @@ import Ajv from "ajv";
 import fs from "fs";
 import yaml from "js-yaml";
 
-const ajv = new Ajv({ allErrors: true, strict: false });
-
-// Step 1: Load and compile the schema
-const schema = yaml.load(fs.readFileSync("src/trial-config.schema.yaml", "utf8"));
-const validate = ajv.compile(schema);
-
-// Step 2: Load the config to validate
-const data = yaml.load(fs.readFileSync("src/trial-config-1.yaml", "utf8"));
-
-// Step 3: Validate
-const valid = validate(data);
-
-if (valid) {
-  console.log("✅ Config is valid!");
-} else {
-  console.error("❌ Config is invalid:");
-  console.error(validate.errors);
+export function validateConfig(configFilePath, logData = false) {
+    const schema = yaml.load(
+        fs.readFileSync("src/trial-config.schema.yaml", "utf8")
+    );
+    
+    const ajv = new Ajv({ allErrors: true, strict: false });
+    const validate = ajv.compile(schema);
+    const data = yaml.load(fs.readFileSync(configFilePath, "utf8"));
+    const valid = validate(data);
+    
+    if (valid) {
+      if (logData) console.log(data);
+      console.log("✅ Config is valid!");
+    } else {
+      console.error("❌ Config is invalid:");
+      console.error(validate.errors);
+    }
 }
